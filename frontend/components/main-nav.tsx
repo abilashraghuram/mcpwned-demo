665c9b38722182feeaaf1f1f1c3d2bb28a4413ee
abilashraghuram <raghuramabilash@gmail.com>
2025-05-26 17:32:08 -0700

    smaller edits

--- frontend/components/main-nav.tsx
+++ frontend/components/main-nav.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link"
-import { Activity } from "lucide-react"
 import { ThemeToggle } from "./theme-toggle"
+import Mlogo from "../public/Mlogo.svg"
 
 export function MainNav() {
 
@@ -9,7 +9,7 @@ export function MainNav() {
     <div className="flex items-center justify-between w-full">
       <div className="flex gap-6 md:gap-10 ml-4">
         <Link href="/" className="flex items-center space-x-2">
-          <Activity className="h-6 w-6" />
+          <Mlogo className="h-6 w-6" />
           <span className="hidden font-bold sm:inline-block">Mcpwned</span>
         </Link>
       </div>
