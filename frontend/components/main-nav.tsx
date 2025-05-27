6368a1512b54838ea737c67305e6f3ff6186a7d9
abilashraghuram <raghuramabilash@gmail.com>
2025-05-26 17:35:45 -0700

    fixing errors

--- frontend/components/main-nav.tsx
+++ frontend/components/main-nav.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link"
+import Image from "next/image"
 import { ThemeToggle } from "./theme-toggle"
-import Mlogo from "../public/Mlogo.svg"
 
 export function MainNav() {
 
@@ -9,7 +9,7 @@ export function MainNav() {
     <div className="flex items-center justify-between w-full">
       <div className="flex gap-6 md:gap-10 ml-4">
         <Link href="/" className="flex items-center space-x-2">
-          <Mlogo className="h-6 w-6" />
+          <Image src="/Mlogo.svg" alt="Mcpwned Logo" width={24} height={24} />
           <span className="hidden font-bold sm:inline-block">Mcpwned</span>
         </Link>
       </div>
